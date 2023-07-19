beb8e7750f5e712fa557be4e0fa4babb1e5e04b3
Matimrs <mati.morales.tepp@gmail.com>
2023-07-19 19:44:58 -0300

    Implementacion objeto Jugador, array de jugadores, y manipulacion del array

--- main.js
+++ main.js
@@ -1,6 +1,79 @@
-//Simulacion de calculo de nivel de un juego X.
+/*  Anotaciones
 
-let partidasJugadas, partidasGanadas, torneosJugados, torneosGanados, flag = true, nivel 
+function Nivel(nA, pG, pJ, tG, tJ, nF){
+    this.nivelActual = nA
+    this.partidasGanadas = pG
+    this.partidasJugadas = pJ
+    this.torneosGanados = tG
+    this.torneosJugados = tJ
+    this.nivelFinal = nF
+}
+
+class Nivel{
+    constructor(nA, pG, pJ, tG, tJ, nF){
+        this.nivelActual = nA
+        this.partidasGanadas = pG
+        this.partidasJugadas = pJ
+        this.torneosGanados = tG
+        this.torneosJugados = tJ
+        this.nivelFinal = nF
+    }
+    metodo(){}
+}
+
+const level = new Nivel (nivelActual, partidasGanadas, partidasJugadas, torneosGanados, torneosJugados, nivelFinal)
+
+*/
+
+
+
+//Simulacion de administrtacion de jugadores de un juego X
+
+let jugadoresActivos = []                                   //Array que contiene los jugadores, inicializado en vacio
+
+function generarID(){                                       //Generacion de un id (identificador de cada jugador) a partir del id del Jugador registrado anteriormente
+    if(jugadoresActivos.length == 0){
+        return 1000001
+    }
+    return ((jugadoresActivos[jugadoresActivos.length - 1].id) + 1)
+}
+class Jugador{                                              //Objeto Jugador, con 4 propiedades
+    constructor(nombre, pais){
+        this.nombre = nombre
+        this.pais = pais
+        this.nivel = 0
+        this.id = generarID()
+    }
+}
+
+function agregarJugador(nombre, pais){                      //Agrega un nuevo Jugador al final del array de jugadoresActivos
+    const aux = new Jugador (nombre,pais)
+    jugadoresActivos.push(aux)
+}
+
+function eliminarJugador(id){                               //Elimina el Jugador del array por su id
+    let pos
+
+    for(let i = 0; i < jugadoresActivos.length ; i++){      //Busca la posicion en el array del jugador que posee dicho id
+
+        if(jugadoresActivos[i].id == id) pos = i
+    }
+
+    if(pos != jugadoresActivos.length) jugadoresActivos.splice(pos, 1)
+}
+
+agregarJugador("Matias","Argentina")                        //Se agrega Jugador ingresando su nombre y pais: agregarJugador(nombre,pais)
+agregarJugador("Lucas","Argentina")
+agregarJugador("Joao","Brasil")
+agregarJugador("Ivan","Argentina")
+
+
+eliminarJugador(1000001)                                    //Se elimina Jugador pasando un id a la funcion: eliminarJugador(id)
+
+console.table(jugadoresActivos)
+
+
+//Simulacion de calculo de nivel de un Jugador Y de un juego X.
 
 function calculo(jug, gan){
 
@@ -13,34 +86,40 @@ function calculo(jug, gan){
     return 0;
 }
 
-while (flag){
+function actualizacionNivel(id, partidasJugadas, partidasGanadas, torneosJugados, torneosGanados){
 
-flag = false;
+    let pos, nivel
 
-nivel = parseInt(prompt("Ingrese su nivel actual: "))
+    for(let i = 0; i < jugadoresActivos.length ; i++){                     //Busca la posicion en el array del jugador que posee dicho id
 
-partidasJugadas = parseInt(prompt("Ingrese la cantidad de partidas jugadas: "))
+        if(jugadoresActivos[i].id == id) pos = i
 
-partidasGanadas = parseInt(prompt("Ingrese la cantidad de partidas ganadas: "))
+    }
 
-torneosJugados = parseInt(prompt("Ingrese la cantidad de torneos jugados: "))
-
-torneosGanados = parseInt(prompt("Ingrese la cantidad de torneos ganados: "))
+    nivel = jugadoresActivos[pos].nivel
 
 
-if(nivel >= 0 && (partidasJugadas >= 0 && (partidasGanadas <= partidasJugadas && (partidasGanadas >= 0 && (torneosJugados >= 0 && (torneosGanados <= torneosGanados && torneosJugados >= 0)))))){
-    
-    nivel += (calculo(partidasJugadas, partidasGanadas) + (partidasJugadas/100))       //calculo de nivel por partidas
+    if(nivel >= 0 && (partidasJugadas >= 0 && (partidasGanadas <= partidasJugadas && (partidasGanadas >= 0 && (torneosJugados >= 0 && (torneosGanados <= torneosGanados && torneosJugados >= 0)))))){
+        
+        nivel += (calculo(partidasJugadas, partidasGanadas) + (partidasJugadas/100))       //Calculo de nivel por partidas
 
-    nivel += (calculo(torneosJugados, torneosGanados) + (partidasGanadas/100)) * 2,5   //calculo de nivel por torneos
+        nivel += (calculo(torneosJugados, torneosGanados) + (partidasGanadas/100)) * 2,5   //Calculo de nivel por torneos
+
+        if(nivel > 100) nivel = 100;
+
+    jugadoresActivos[pos].nivel = nivel
+
+    }                                                        //Si no se cumple algunas de las condiciones, no se actualiza el nivel debido a que hay un error
 }
-else {
-    alert("Se ha ingresado un campo incorrectamente, vuelve a ingresar")
 
-    flag = true;
-}
-}
-if(nivel > 100) nivel = 100;
+let id = jugadoresActivos[jugadoresActivos.length - 1].id, pj = 200, pg = 120, tj = 50, tg = 30
 
-alert("Su nivel actualizado es: "+ parseInt(nivel))
+actualizacionNivel(id, pj, pg, tj, tg)                       //Se actualiza el nivel del ultimo Jugador del array, con la nueva informacion de partidas y torneos                                                
+
+agregarJugador("Mateo","Chile")
+
+console.table(jugadoresActivos)
+
+
+
 
