b339b8fc30963aecf19568f29737dba555a32faf
Matimrs <mati.morales.tepp@gmail.com>
2023-08-02 22:47:53 -0300

    Implementacion de busqueda (creacion y eliminacion de nodos)

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //Simulacion de administrtacion de jugadores de un juego X
 
-
 function generarID(){                                                   //Generacion de un id (identificador de cada jugador) a partir del id del Jugador registrado anteriormente
     let get = localStorage.getItem("jugadores"), aux = JSON.parse(get)
     if(get === null || aux.length == 0) return 1000001
@@ -16,8 +15,8 @@ class Jugador{                                                          //Objeto
 }
 
 function agregarJugador(nombre, pais){                                  //Agrega un nuevo Jugador al final del array de jugadoresActivos
-    const aux = new Jugador (nombre,pais), get = localStorage.getItem("jugadores")
-    let aux1
+    const aux = new Jugador (nombre,pais)
+    let aux1, get = localStorage.getItem("jugadores")
     if(get == null) aux1 = [aux]
     else {
         aux1 = JSON.parse(get)
@@ -27,14 +26,19 @@ function agregarJugador(nombre, pais){                                  //Agrega
 }
 
 function eliminarJugador(id){                                           //Elimina el Jugador del array por su id
-    let pos, aux = JSON.parse(localStorage.getItem("jugadores"))
-
-    for(let i = 0; i < aux.length ; i++){                               //Busca la posicion en el array del jugador que posee dicho id
-        if(aux[i].id == id) pos = i
+    let pos, aux = JSON.parse(localStorage.getItem("jugadores")), i = 0
+    let flag = true;
+    while( i < aux.length && flag){                                             //Busca la posicion en el array del jugador que posee dicho id
+        if(aux[i].id == id) {
+            pos = i
+            flag = false
+        }
+        i++
     }
-
-    aux.splice(pos, 1)
-    localStorage.setItem("jugadores",JSON.stringify(aux))
+    if(pos != undefined){
+        aux.splice(pos, 1)
+        localStorage.setItem("jugadores",JSON.stringify(aux))
+    }
 }
 
 //Simulacion de calculo de nivel de un Jugador Y de un juego X.
@@ -81,6 +85,7 @@ let id = 1000002, pj = 200, pg = 120, tj = 50, tg = 30
 
 //actualizacionNivel(id, pj, pg, tj, tg)                                    //Se actualiza el nivel del ultimo Jugador del array, con la nueva informacion de partidas y torneos                                                
 
+//Agregar jugador:
 
 let btnAgregarJugador = document.getElementById("nuevo-jugador-enviar")
 
@@ -88,9 +93,14 @@ btnAgregarJugador.addEventListener("click",nuevoJugador)
 
 function nuevoJugador(){
     const nombre = document.getElementById("nuevo-jugador-nombre").value, pais = document.getElementById("nuevo-jugador-pais").value
-    agregarJugador(nombre, pais)
+    if(nombre == "") alert("Nombre sin completar")
+    else if(pais == "") alert("Pais sin completar")
+    else{
+        agregarJugador(nombre, pais)
+    }
 }
 
+//Eliminar Jugador:
 
 let btnEliminarJugador = document.getElementById("eliminar-jugador-enviar")
 
@@ -101,7 +111,57 @@ function borrarJugador(){
     eliminarJugador(id)
 }
 
+//Buscar jugador:
 
+let btnBuscarJugador = document.getElementById("buscar-amigos-enviar")
+
+btnBuscarJugador.addEventListener("click",buscarJugador)
+
+function buscarJugador(){
+    let aux = JSON.parse(localStorage.getItem("jugadores")), nombre = document.getElementById("buscar-amigos").value, mostrar
+    let flag = false
+    for(let i = 0; i < aux.length ; i++){
+        if(aux[i].nombre == nombre){
+            flag = true
+            mostrar = document.createElement("div")
+            mostrar.className = 'boton-agregar-amigo'
+            mostrar.innerHTML = "<h3>Nombre " + aux[i].nombre +"</h3> <h3>Nivel "+ aux[i].nivel +"</h3> <h3>Pais "+ aux[i].pais +"</h3> <button id='agregar-amigo'>Agregar amigo</button>"
+            let divLista = document.getElementById("lista-busqueda")
+            divLista.append(mostrar)
+        }
+    }
+    let divBuscar = document.getElementById("busqueda")
+    if(!flag){
+        let aviso
+        aviso = document.createElement("p")
+        aviso.id = "aviso-no-coincidencia"
+        aviso.innerText = "No se encontraron coincidencias"
+        divBuscar.append(aviso)
+    }
+    let cancelar
+    cancelar = document.createElement("button")
+    cancelar.id = 'cancelar-busqueda'
+    cancelar.innerText = "Salir"
+    divBuscar.append(cancelar)
+    cancelar.addEventListener("click",borrarBusqueda)
+}
+
+function borrarBusqueda(){
+    let arrayBusqueda = document.querySelectorAll(".boton-agregar-amigo"), btnCancelar = document.getElementById("cancelar-busqueda"), aviso = document.getElementById("aviso-no-coincidencia")
+    for(let i = 0 ; i < arrayBusqueda.length ; i++){
+        arrayBusqueda[i].remove()
+    }
+    aviso.remove()
+    btnCancelar.remove()
+}
+
+let btnAgregarAmigo = document.getElementById("agregar-amigo")
+
+btnAgregarAmigo.addEventListener("click",agregarAmigo)
+
+function agregarAmigo(){
+
+}
 
 
 
